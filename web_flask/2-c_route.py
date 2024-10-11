d92dffb382d6296269da5d69ca6e539c808d00ee
LassyGAKUBA <l.gakuba1@alustudent.com>
2024-10-11 03:43:36 -0700

    string

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -27,7 +27,7 @@ def hbnb():
 def c(text):
     """Displays 'C' followed by the value of <text>."""
     text = text.replace("_", " ")
-    return "C {}".format(text)
+    return f"C {text}"
 
 
 if __name__ == "__main__":
